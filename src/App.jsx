5cc1d754207e58c7a8e34636f654da8a5003ad3d
Thomas <thomasfindlay94@gmail.com>
2022-07-11 17:37:57 +0100

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,37 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { Upload } from "@progress/kendo-react-upload";
+
+const saveUrl = "https://demos.telerik.com/kendo-ui/service-v4/upload/save";
+const removeUrl = "https://demos.telerik.com/kendo-ui/service-v4/upload/remove";
+
+const UploadedFileItem = props => {
+  console.log("props", props);
+  return (
+    <ul>
+      {props.files.map(file => (
+        <li key={file.name}>{file.name}</li>
+      ))}
+    </ul>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Upload
+        defaultFiles={[]}
+        withCredentials={false}
+        saveUrl={saveUrl}
+        removeUrl={removeUrl}
+        restrictions={{
+          minFileSize: 1000,
+          maxFileSize: 10000000,
+          allowedExtensions: [".jpg", ".png"],
+        }}
+        listItemUI={UploadedFileItem}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
